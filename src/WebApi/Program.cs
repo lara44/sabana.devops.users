5326c0db4b30fde798fc898d6d361cc413fa015b
lara44 <43456256+lara44@users.noreply.github.com>
2026-02-22 17:58:16 -0500

    [REMOVE] comment out HTTPS redirection in Program.cs

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/", () => "Microservices users DevOps!");
 app.MapGet("/health", () => "Healthy");
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.MapControllers();
 
