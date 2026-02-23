1e90d7e5147f7314217ca332f0f861c7e935f577
lara44 <43456256+lara44@users.noreply.github.com>
2026-02-22 19:01:33 -0500

    configure Kestrel to listen on HTTP only to prevent shutdown due to lack of HTTPS

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -5,6 +5,12 @@ using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// FORZAR Kestrel a escuchar solo en HTTP para evitar el cierre por falta de HTTPS
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080); 
+});
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
